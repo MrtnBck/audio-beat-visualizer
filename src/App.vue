fe31576741553a9d7709b3ee6f19cd7aa4087aea
Martin Bock <martinbock@macair.local>
2022-12-16 21:23:57 +0100

    add UI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <header>Audio Beat Visualizer</header>
+  <section>
+    <div class="circle"></div>
+  </section>
 </template>
 
 <script>
@@ -8,4 +10,25 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+body {
+  margin: 0;
+}
+
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(0deg, #485563, #29323c);
+}
+
+section .circle {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background: linear-gradient(0deg, #fe8c00, #f83600);
+  box-shadow: 0px 0px 30px #0f1316;
+}
+</style>
